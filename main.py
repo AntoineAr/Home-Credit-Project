ddb0c1641e30f602defd587a8e4d1f850ed89baa
AntoineAr <arragon.antoine@gmail.com>
2024-05-14 20:10:16 +0200

    Modifs main

--- main.py
+++ main.py
@@ -143,8 +143,9 @@ def global_shap():
 def local_shap(customer_id):
     if customer_id in clients_ids:
         client_data = features.loc[customer_id].values.reshape(1, -1)
+        client_index = features.index.get_loc(customer_id)
         shap.force_plot(explainer.expected_value, 
-                        shap_values[1][customer_id],
+                        shap_values[1][client_index],
                         client_data.round(2), 
                         feature_names=features.columns, 
                         matplotlib=True, 
